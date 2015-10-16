6736104a3d0f3ea023e8fde936a0e5f2fcc50d7f
Glen Peek <glen.peek@gmail.com>
2015-10-17 00:33:34 +1300

    [create] tap to start scene
    users now begin the game on a title screen that they must tap to start
    the game

---
+++ Assets/TapToStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using UnityEngine.EventSystems;
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+	public class TapToStart : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+	{
+		
+		public void OnPointerDown(PointerEventData data)
+		{
+		}
+		
+		
+		public void OnPointerUp(PointerEventData data)
+		{
+			Application.LoadLevel ("OnTriggerTester");
+		}
+	}
+}
