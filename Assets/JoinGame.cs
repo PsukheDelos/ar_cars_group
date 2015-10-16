51616b6fb8b419a3669f422d4a3ff57ab7fdedbf
Glen Peek <glen.peek@gmail.com>
2015-10-17 10:44:46 +1300

    shows some messages when connecting to game

--- Assets/JoinGame.cs
+++ Assets/JoinGame.cs
@@ -9,7 +9,10 @@ namespace UnityStandardAssets.CrossPlatformInput{
 	public class JoinGame : Photon.MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
 
 		int roomNo = 0;
+		private bool join = true;
 		private float timer = 0.0f;
+		public GUISkin Skin;
+
 
 		/// <summary>Connect automatically? If false you can set this to true later on or call ConnectUsingSettings in your own scripts.</summary>
 		public bool AutoConnect = true;
@@ -18,7 +21,6 @@ namespace UnityStandardAssets.CrossPlatformInput{
 		
 		/// <summary>if we don't want to connect in Start(), we have to "remember" if we called ConnectUsingSettings()</summary>
 		private bool ConnectInUpdate = true;
-		private bool once = true;
 
 		public Dropdown lobbies;
 		// Use this for initialization
@@ -30,10 +32,6 @@ namespace UnityStandardAssets.CrossPlatformInput{
 		void Update () {
 			timer += Time.deltaTime;
 			if (timer >= 1.0f) {
-//				if(PhotonNetwork.insideLobby && once){
-////					Debug.Log("TEST: " + PhotonNetwork.GetRoomList().GetValue(0).ToString);
-//					once = false;
-//				}
 				Dropdown.OptionData od = new Dropdown.OptionData();
 				od.text = "[New Game]";
 				lobbies.options.Clear();
@@ -51,7 +49,7 @@ namespace UnityStandardAssets.CrossPlatformInput{
 			if (ConnectInUpdate && AutoConnect && !PhotonNetwork.connected)
 			{
 				Debug.Log("Update() was called by Unity. Scene is loaded. Let's connect to the Photon Master Server. Calling: PhotonNetwork.ConnectUsingSettings();");
-				
+				Debug.Log (Version + "."+Application.loadedLevel);
 				ConnectInUpdate = false;
 				PhotonNetwork.ConnectUsingSettings(Version + "."+Application.loadedLevel);
 			}
@@ -63,14 +61,22 @@ namespace UnityStandardAssets.CrossPlatformInput{
 		public void OnPointerDown(PointerEventData data)
 		{
 //			PhotonNetwork.CreateRoom("ar_room", new RoomOptions() { maxPlayers = 2 }, null);
+			join = true;
+			PhotonNetwork.JoinOrCreateRoom ("ar_room", new RoomOptions () { maxPlayers = 2 }, null);
+//			PhotonNetwork.JoinRandomRoom ();
+//			if (lobbies.value == 0) {
+//				bool createRoom =  PhotonNetwork.CreateRoom ("ar_room", new RoomOptions() { maxPlayers = 2 }, null);
+//				Debug.Log ("Create Room: " + createRoom.ToString());
+//			} else {
+//				PhotonNetwork.JoinRoom(lobbies.options[lobbies.value].text);
+//				Debug.Log ("Join Room");
+//			}
 
-			if (lobbies.value == 0) {
-				bool createRoom =  PhotonNetwork.CreateRoom ("Booger", new RoomOptions() { maxPlayers = 2 }, null);
-				Debug.Log ("Create Room: " + createRoom.ToString());
-			} else {
-				PhotonNetwork.JoinRoom(lobbies.options[lobbies.value].text);
-				Debug.Log ("Join Room");
-			}
+//			if( PhotonNetwork.connectionStateDetailed == PeerState.Joined )
+//			{
+				GameObject.Find ("JoinCanvas").gameObject.GetComponent<Canvas> ().enabled = false;
+				GameObject.Find ("ButtonCanvas").gameObject.GetComponent<Canvas> ().enabled = true;
+//			}
 //			Debug.Log ("Lobby Selection: " + lobbies.options[lobbies.value].text);
 
 //			lobbies.options.Clear;
@@ -109,12 +115,53 @@ namespace UnityStandardAssets.CrossPlatformInput{
 		
 		public void OnJoinedRoom()
 		{
+			Debug.Log ("HEYHEYHYE");
 			Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room. From here on, your game would be running. For reference, all callbacks are listed in enum: PhotonNetworkingMessage");
 		}
 		
 		public void OnJoinedLobby()
 		{
 			Debug.Log("OnJoinedLobby(). Use a GUI to show existing rooms available in PhotonNetwork.GetRoomList().");
+		}
+
+
+		void OnGUI()
+		{
+			if( Skin != null )
+			{
+				GUI.skin = Skin;
+			}
+			
+			float width = 400;
+			float height = 100;
+			if (join) {
+//				Rect centeredRect = new Rect ((Screen.width - width) / 2, (Screen.height - height) / 2, width, height);
+				Rect centeredRect = new Rect (0, 0, width, height);
+
+				GUILayout.BeginArea (centeredRect, GUI.skin.box);
+				{
+					GUILayout.Label ("Connecting" + GetConnectingDots (), GUI.skin.customStyles [0]);
+					GUILayout.Label ("Status: " + PhotonNetwork.connectionStateDetailed);
+				}
+				GUILayout.EndArea ();
+			}
+			if( PhotonNetwork.connectionStateDetailed == PeerState.Joined )
+			{
+				enabled = false;
+			}
+		}
+
+		string GetConnectingDots()
+		{
+			string str = "";
+			int numberOfDots = Mathf.FloorToInt( Time.timeSinceLevelLoad * 3f % 4 );
+			
+			for( int i = 0; i < numberOfDots; ++i )
+			{
+				str += " .";
+			}
+			
+			return str;
 		}
 	}
 }
