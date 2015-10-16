c8a57639ce60342dce0c25e42b7124a529a977fd
Glen Peek <glen.peek@gmail.com>
2015-10-17 03:24:53 +1300

    makes "tap to start" blink

--- Assets/TapToStart.cs
+++ Assets/TapToStart.cs
@@ -6,8 +6,32 @@ using UnityEngine.EventSystems;
 
 namespace UnityStandardAssets.CrossPlatformInput
 {
+
+
 	public class TapToStart : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 	{
+
+		private Text _material ;
+		private float timer = 0.0f;
+
+		void OnEnable () {
+			_material = GetComponent<Text> ();
+			_material.color = Color.white;
+		}
+
+		public void Update(){
+			timer += Time.deltaTime;
+			if (timer >= 0.5f)//change the float value here to change how long it takes to switch.
+			{
+				if(_material.color==Color.white){
+					_material.color = Color.yellow;
+				}
+				else{
+					_material.color = Color.white;
+				}
+				timer = 0;
+			}
+		}
 		
 		public void OnPointerDown(PointerEventData data)
 		{
