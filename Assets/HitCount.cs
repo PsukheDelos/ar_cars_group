f3dd133c237cf4019759373bedf9e90eca4be468
metuartuak <tui12593@gmail.com>
2015-10-18 14:03:56 +1300

    Destructive Meshes Working/Environmental Assets Beginning
    See Above

--- Assets/HitCount.cs
+++ Assets/HitCount.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 
 public class HitCount : MonoBehaviour{
-	public Transform Car;
+	public Transform Bomb;
 
 
 	int hit = 0;
@@ -12,16 +12,21 @@ public class HitCount : MonoBehaviour{
 
 
 void OnCollisionEnter(Collision col){
-		if (col.gameObject.name == "Car(Clone)") {
+		if (col.gameObject.name == "Cube") {
 			Debug.Log ("Hit");
 			hit += 1;
 			checkhit ();
+		}
+		if (col.gameObject.name == "Rocket") {
+				Debug.Log ("Hit");
+				hit = 3;
+				checkhit ();
 		}
 	}
 void checkhit(){
 	if(hit == 3){
 		Destroy(gameObject);
-			Instantiate(Car, transform.position, transform.rotation);
+			Instantiate(Bomb, transform.position, transform.rotation);
 	}
   }
 }
