3d9d63d3c18bf14a6bb06ac4b50d0e2cda665d18
Glen Peek <glen.peek@gmail.com>
2015-10-29 11:09:24 +1300

    added in death animations

--- Assets/Scripts/Legacy/HitCount2.cs
+++ Assets/Scripts/Legacy/HitCount2.cs
@@ -15,16 +15,18 @@ void OnCollisionEnter(Collision col){
 		if (col.gameObject.name.Contains ("Bullet")) {
 			Debug.Log ("Bullet Hit");
 			hit += 1;
-		} else if (col.gameObject.tag=="Player"){
+		} else if (col.gameObject.tag == "Player") {
 			Debug.Log ("Vehicle Hit");
 			hit += 5;
-		}
+		} 
+		hit += 5; //for debugging
 		checkhit ();
 	}
 void checkhit(){
 	if(hit == 5){
 		Destroy(gameObject);
 //		PhotonNetwork.Instantiate (Car, transform.position, transform.rotation, 0);
+			// might need to do this over the network???
 		Instantiate(Car, transform.position, transform.rotation);
 	}
   }
