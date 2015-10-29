15629487ef09949e849cdc406ca92b5e1a5245e1
Glen Peek <glen.peek@gmail.com>
2015-10-29 18:17:42 +1300

    added in health bar

--- Assets/Scripts/Legacy/HitCount2.cs
+++ Assets/Scripts/Legacy/HitCount2.cs
@@ -1,36 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 
 
 public class HitCount2 : MonoBehaviour{
 	public Transform DeathAnimation;
 
-
-	int hit = 0;
+	int health = 100;
 
 
 void OnCollisionEnter(Collision col){
 		if (col.gameObject.name.Contains ("Bullet")) {
 			Debug.Log ("Bullet Hit");
-			hit += 1;
+			health -= 5;
 		
 		} else if (col.gameObject.tag == "Ram") {
 			Debug.Log ("Vehicle Hit");
-			hit += 3;
+			health -= 10;
 		} 
 		GameObject.Find(PhotonNetwork.playerName).GetPhotonView().RPC("checkhit", PhotonTargets.All);
+		GameObject.Find ("HealthSlider").GetComponent<Slider> ().value = health;
 	}
 
 [PunRPC]
 	void checkhit(){
-	if(hit == 20){
-		GameObject spawnPoint = GameObject.Find ("Player" + PhotonNetwork.playerName + "SpawnPoint");
-		PhotonNetwork.Instantiate(DeathAnimation.name, transform.position, transform.rotation, 0);
-		gameObject.transform.position = spawnPoint.transform.position;
-		gameObject.transform.rotation = spawnPoint.transform.rotation;
-		hit = 0;
-		PhotonNetwork.player.AddScore(1);
+		if(health == 0){
+			GameObject spawnPoint = GameObject.Find ("Player" + PhotonNetwork.playerName + "SpawnPoint");
+			PhotonNetwork.Instantiate(DeathAnimation.name, transform.position, transform.rotation, 0);
+			gameObject.transform.position = spawnPoint.transform.position;
+			gameObject.transform.rotation = spawnPoint.transform.rotation;
+			health = 100;
+			GameObject.Find ("HealthSlider").GetComponent<Slider> ().value = health;
+			PhotonNetwork.player.AddScore(1);
 		}
-  }
+  	}
 }
